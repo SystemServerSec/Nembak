f40ee083f3a6110d6cd0d1396e67efcacc516c1d
Khusaeni <43690001+SystemServerSec@users.noreply.github.com>
2019-10-04 19:06:42 +0700

    Create nembak.py

---
+++ nembak.py
@@ -0,0 +1,12 @@
+import os, time, random
+
+os.system("echo selama bertahun-bertahun aku mencintai kamu, andai saja menjadi takdir kita besok... maukah aku menjadi kekasihmu? | lolcat")
+print("")
+test = input("pilih aku atau dia? (1/2) = ")
+
+if test == 1:
+    print("terima kasih")
+if test == 2:
+     print("ok gapapa aku ikhlas kok")
+else:
+     print("\033[31;1mwrong input!!!")
