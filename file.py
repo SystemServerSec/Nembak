bf146bfbfbf33e521f7fb445dee5a06869b094c8
MicroClone <43690001+tanwirulqulub999@users.noreply.github.com>
2021-07-22 20:44:30 +0700

    Update and rename nembak.py to file.py

---
+++ file.py
@@ -0,0 +1,12 @@
+import os, time, random
+
+os.system("echo developer or pentester | lolcat")
+print("")
+test = input("select (1/2) = ")
+
+if test == 1:
+    print("good job!")
+if test == 2:
+     print("nice")
+else:
+     print("\033[31;1mwrong input!!!")
